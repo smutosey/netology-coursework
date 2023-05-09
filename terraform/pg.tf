1f16016895d07b13812b872f1f94824e78dcd985
alexander.nedorezov <alserned@gmail.com>
2023-05-09 17:15:41 +0700

    postgres subnets name fix

--- terraform/pg.tf
+++ terraform/pg.tf
@@ -70,12 +70,14 @@ resource "yandex_mdb_postgresql_user" "prom" {
 
 
 resource "yandex_vpc_subnet" "private-db-a" {
+  name           = "private-db-a"
   zone           = "ru-central1-a"
   network_id     = yandex_vpc_network.vpc.id
   v4_cidr_blocks = ["10.1.0.0/24"]
 }
 
 resource "yandex_vpc_subnet" "private-db-b" {
+  name           = "private-db-b"
   zone           = "ru-central1-b"
   network_id     = yandex_vpc_network.vpc.id
   v4_cidr_blocks = ["10.2.0.0/24"]
